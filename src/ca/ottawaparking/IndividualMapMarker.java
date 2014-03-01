9bee96a5c0c09e01d0d0bf2be9204aec0e44cbb0
AlexGenio <rob_b-boy_seb@hotmail.com>
2014-03-01 16:02:26 -0500

    Pushing mapview

---
+++ src/ca/ottawaparking/IndividualMapMarker.java
@@ -0,0 +1,24 @@
+package ca.ottawaparking;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class IndividualMapMarker extends FragmentActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mapmarker);
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
