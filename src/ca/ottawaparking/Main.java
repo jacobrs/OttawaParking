a890f531f18d66fd5bc36b926de7ebe03b21ab0c
AlexGenio <rob_b-boy_seb@hotmail.com>
2014-02-28 22:17:32 -0500

    MENUS CREATED

--- src/ca/ottawaparking/Main.java
+++ src/ca/ottawaparking/Main.java
@@ -8,7 +8,10 @@ package ca.ottawaparking;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
 
@@ -16,6 +19,44 @@ public class Main extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button bikeActivity = (Button)findViewById(R.id.btnBike);
+		Button carActivity = (Button)findViewById(R.id.btnCar);
+		Button stubActivity = (Button)findViewById(R.id.btnStub);
+		
+		bikeActivity.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openBikeMenu = new Intent("ca.ottawaparking.BIKE");
+				startActivity(openBikeMenu);
+
+			}
+		});
+		
+		carActivity.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openCarMenu = new Intent("ca.ottawaparking.CAR");
+				startActivity(openCarMenu);
+				
+			}
+		});
+
+		stubActivity.setOnClickListener(new View.OnClickListener() {
+	
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openStubMenu = new Intent("ca.ottawaparking.STUB");
+				startActivity(openStubMenu);
+				
+			}
+		});
+		
 	}
 
 	@Override
