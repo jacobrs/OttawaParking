edbb6fa909fa27c7d9eb6427321cd2b3872e8a73
benrs <benjaminbarault@gmail.com>
2014-03-01 00:01:58 -0500

    Fixed splash and going back

--- src/ca/ottawaparking/Main.java
+++ src/ca/ottawaparking/Main.java
@@ -8,7 +8,9 @@ package ca.ottawaparking;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -75,6 +77,22 @@ public class Main extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+	    new AlertDialog.Builder(this)
+	        .setTitle("Really Exit?")
+	        .setMessage("Are you sure you want to exit?")
+	        .setNegativeButton(android.R.string.no, null)
+	        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+	            public void onClick(DialogInterface arg0, int arg1) {
+	                Main.super.onBackPressed();
+	                finish();
+	                System.exit(1);
+	            }
+	        }).create().show();
 	}
 
 }
