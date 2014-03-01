4e4df8f0a6260b0ba7ee68b082c4ab1f88c37fac
benrs <benjaminbarault@gmail.com>
2014-02-28 21:24:59 -0500

    Fixed the splash

--- src/ca/ottawaparking/Splash.java
+++ src/ca/ottawaparking/Splash.java
@@ -5,7 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity {
-
+	boolean paused;
+	
 	@Override
 	protected void onCreate(Bundle createSplash) {
 		super.onCreate(createSplash);
@@ -19,9 +20,43 @@ public class Splash extends Activity {
 					e.printStackTrace();
 				}
 				finally {
-					//intent based off of action name
-					Intent openMainActivity = new Intent("ca.ottawaparking.MAIN");
-					startActivity(openMainActivity);	//will open activity based off of intent
+					if(!paused){ //Make sure person didn't click home button
+						//intent based off of action name
+						Intent openMainActivity = new Intent("ca.ottawaparking.MAIN");
+						startActivity(openMainActivity);	//will open activity based off of intent
+					}
+				}
+			}
+		};
+		timer.start();	//start thread
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		paused = true;
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		paused = false;
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(2000);	//pauses thread using milliseconds
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				finally {
+					if(!paused){
+						//intent based off of action name
+						Intent openMainActivity = new Intent("ca.ottawaparking.MAIN");
+						startActivity(openMainActivity);	//will open activity based off of intent
+					}
 				}
 			}
 		};
