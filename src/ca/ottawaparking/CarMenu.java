7d54562a8f311cbd8cb22bbef85b6a9d5882809f
benrs <benjaminbarault@gmail.com>
2014-03-02 13:02:16 -0500

    Re-generate lists

--- src/ca/ottawaparking/CarMenu.java
+++ src/ca/ottawaparking/CarMenu.java
@@ -38,6 +38,7 @@ public class CarMenu extends FragmentActivity{
 	final int AMOUNT_OF_VIEWS = 15;
 	GoogleMap map;
 	GoogleMap mapall;
+	ArrayList<String> list;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -133,7 +134,7 @@ public class CarMenu extends FragmentActivity{
 		
 		// Get ListView object from xml
         listView = (ListView) findViewById(R.id.list);
-     // Defined Array values to show in ListView
+        // Defined Array values to show in ListView
         String[] values = new String[AMOUNT_OF_VIEWS+1];
         
         int counter = 0;
@@ -151,7 +152,7 @@ public class CarMenu extends FragmentActivity{
         // Second parameter - Layout for the row
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
-        final ArrayList<String> list = new ArrayList<String>(Arrays.asList(values));
+        list = new ArrayList<String>(Arrays.asList(values));
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
        
         // Assign adapter to ListView
@@ -258,7 +259,8 @@ public class CarMenu extends FragmentActivity{
 					Location lastknownloc = locationManager.getLastKnownLocation(provider);
 					if(lastknownloc != null){
 						// next line makes you Ottawanian
-						// lastknownloc.setLatitude((double)45.214);lastknownloc.setLongitude((double)-75.6919);
+						//lastknownloc.setLatitude((double)45.214);
+						//lastknownloc.setLongitude((double)-75.6919);
 						ourStack.CarSort(lastknownloc);
 						final int points = 4;
 						int count = 0;
@@ -293,8 +295,80 @@ public class CarMenu extends FragmentActivity{
 						}
 					}
 				}
-			
-				
+				amtIteration = 0;
+				// Get ListView object from xml
+		        listView = (ListView) findViewById(R.id.list);
+		        // Defined Array values to show in ListView
+		        String[] values = new String[AMOUNT_OF_VIEWS+1];
+		        
+		        int counter = 0;
+		        
+		        for(; counter < AMOUNT_OF_VIEWS; counter++){
+		        	values[counter] = ourStack.getElement(ourStack.getTopIndex() - counter).getAddress();
+		        	System.out.println(values[counter]);
+		        }
+		        amtIteration++;
+		        
+		        values[AMOUNT_OF_VIEWS] = "View More";
+		        
+		        // Define a new Adapter
+		        // First parameter - Context
+		        // Second parameter - Layout for the row
+		        // Third parameter - ID of the TextView to which the data is written
+		        // Forth - the Array of data
+		        list = new ArrayList<String>(Arrays.asList(values));
+		        adapter = new ArrayAdapter<String>(CarMenu.this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+		       
+		        // Assign adapter to ListView
+		        listView.setAdapter(adapter);
+		        
+		     // ListView Item Click Listener
+		        listView.setOnItemClickListener(new OnItemClickListener() {
+		        	
+		              @Override
+		              public void onItemClick(AdapterView<?> parent, View view,
+		                 int position, long id) {
+		                
+		            	  // ListView Clicked item index
+			              int itemPosition     = position;
+			              
+			              // ListView Clicked item value
+			              String  itemValue    = (String) listView.getItemAtPosition(position);
+			                 
+		                  // Show Alert 
+		                  /* 
+		                  Toast.makeText(getApplicationContext(),
+		                     "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+		                     .show();
+		                  */
+			              if(position%(AMOUNT_OF_VIEWS*amtIteration) == 0 && position != 0 && position < ourStack.getTopIndex()){
+		                   	list.remove(position);
+		                   	adapter.notifyDataSetChanged();
+		                   	
+		                   	String[] newValues = new String[AMOUNT_OF_VIEWS+1];
+		                   	int newCounter = 0;
+		                   	
+		                   	for(; newCounter < AMOUNT_OF_VIEWS && (position + newCounter) < ourStack.getTopIndex(); newCounter++){
+		                		newValues[newCounter] = ourStack.getElement(ourStack.getTopIndex() - ((amtIteration*AMOUNT_OF_VIEWS) + newCounter)).getAddress();
+		                		list.add((amtIteration*AMOUNT_OF_VIEWS)+newCounter, newValues[newCounter]);
+		                	}
+		                	
+		                   	if(!(position+newCounter >= ourStack.getTopIndex())){
+		                		newValues[newCounter] = "View More";
+		                		list.add((amtIteration*AMOUNT_OF_VIEWS)+newCounter, newValues[newCounter]);
+		                	}
+		                	amtIteration++;
+		                	adapter.notifyDataSetChanged();
+		                }else if(!(position%(AMOUNT_OF_VIEWS*amtIteration) == 0) || position == 0){
+		                	//start new fragment activity
+		                	Intent openMapMarker = new Intent(CarMenu.this, IndividualMapMarker.class);
+		    				openMapMarker.putExtra("longitude", ourStack.getElement(ourStack.getTopIndex() - (position)).getLong());
+		    				openMapMarker.putExtra("latitude", ourStack.getElement(ourStack.getTopIndex() - (position)).getLat());
+		    				openMapMarker.putExtra("location", ourStack.getElement(ourStack.getTopIndex() - (position)).getAddress());
+		    				startActivity(openMapMarker);
+		                }
+		             }
+		         }); 
 			}
 		});
 	}
--- src/ca/ottawaparking/StubMenu.java
+++ src/ca/ottawaparking/StubMenu.java
@@ -35,6 +35,7 @@ public class StubMenu extends Activity{
 	final int AMOUNT_OF_VIEWS = 15;
 	GoogleMap map;
 	GoogleMap mapall;
+	ArrayList<String> list;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -144,7 +145,7 @@ public class StubMenu extends Activity{
         // Second parameter - Layout for the row
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
-        final ArrayList<String> list =new ArrayList<String>(Arrays.asList(values));
+        list =new ArrayList<String>(Arrays.asList(values));
         adapter = new ArrayAdapter<String>(this,
           android.R.layout.simple_list_item_1, android.R.id.text1, list);
         // Assign adapter to ListView
@@ -282,6 +283,81 @@ public class StubMenu extends Activity{
 						}
 					}
 				}
+				
+				amtIteration = 0;
+				// Get ListView object from xml
+		        listView = (ListView) findViewById(R.id.list);
+		        // Defined Array values to show in ListView
+		        String[] values = new String[AMOUNT_OF_VIEWS+1];
+		        
+		        int counter = 0;
+		        
+		        for(; counter < AMOUNT_OF_VIEWS; counter++){
+		        	values[counter] = ourStack.getElement(ourStack.getTopIndex() - counter).get_address();
+		        	System.out.println(values[counter]);
+		        }
+		        amtIteration++;
+		        
+		        values[AMOUNT_OF_VIEWS] = "View More";
+		        
+		        // Define a new Adapter
+		        // First parameter - Context
+		        // Second parameter - Layout for the row
+		        // Third parameter - ID of the TextView to which the data is written
+		        // Forth - the Array of data
+		        list = new ArrayList<String>(Arrays.asList(values));
+		        adapter = new ArrayAdapter<String>(StubMenu.this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+		       
+		        // Assign adapter to ListView
+		        listView.setAdapter(adapter);
+		        
+		     // ListView Item Click Listener
+		        listView.setOnItemClickListener(new OnItemClickListener() {
+		        	
+		              @Override
+		              public void onItemClick(AdapterView<?> parent, View view,
+		                 int position, long id) {
+		                
+		            	  // ListView Clicked item index
+			              int itemPosition     = position;
+			              
+			              // ListView Clicked item value
+			              String  itemValue    = (String) listView.getItemAtPosition(position);
+			                 
+		                  // Show Alert 
+		                  /* 
+		                  Toast.makeText(getApplicationContext(),
+		                     "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+		                     .show();
+		                  */
+			              if(position%(AMOUNT_OF_VIEWS*amtIteration) == 0 && position != 0 && position < ourStack.getTopIndex()){
+		                   	list.remove(position);
+		                   	adapter.notifyDataSetChanged();
+		                   	
+		                   	String[] newValues = new String[AMOUNT_OF_VIEWS+1];
+		                   	int newCounter = 0;
+		                   	
+		                   	for(; newCounter < AMOUNT_OF_VIEWS && (position + newCounter) < ourStack.getTopIndex(); newCounter++){
+		                		newValues[newCounter] = ourStack.getElement(ourStack.getTopIndex() - ((amtIteration*AMOUNT_OF_VIEWS) + newCounter)).get_address();
+		                		list.add((amtIteration*AMOUNT_OF_VIEWS)+newCounter, newValues[newCounter]);
+		                	}
+		                	
+		                   	if(!(position+newCounter >= ourStack.getTopIndex())){
+		                		newValues[newCounter] = "View More";
+		                		list.add((amtIteration*AMOUNT_OF_VIEWS)+newCounter, newValues[newCounter]);
+		                	}
+		                	amtIteration++;
+		                	adapter.notifyDataSetChanged();
+		                }else if(!(position%(AMOUNT_OF_VIEWS*amtIteration) == 0) || position == 0){
+		                	//start new fragment activity
+		                	Intent openMapMarker = new Intent(StubMenu.this, IndividualMapMarker.class);
+		    				openMapMarker.putExtra("longitude", ourStack.getElement(ourStack.getTopIndex() - (position)).get_longitude());
+		    				openMapMarker.putExtra("latitude", ourStack.getElement(ourStack.getTopIndex() - (position)).get_latitude());
+		    				openMapMarker.putExtra("location", ourStack.getElement(ourStack.getTopIndex() - (position)).get_address());
+		    				startActivity(openMapMarker);
+		                }
+		             }
+		         }); 
 			}
 		});
 	}
